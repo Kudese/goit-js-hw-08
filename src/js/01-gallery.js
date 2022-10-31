f55eb326e2c16d9d5e94ca09005ed4cf156cebc7
Kudese <d.kuprichenkov@gmail.com>
2022-10-31 02:55:10 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox"
+
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+const elGallery = document.querySelector(".gallery");
+
+//Create gallary in DOM
+let gallaryList = "";
+for (let i = 0; i < galleryItems.length; i++) {
+  const { preview, original, description } = galleryItems[i];
+  gallaryList += `<li><a classs='gallery__item' href='${original}' >
+    <img class="gallery__image"  src="${preview}" alt="${description}">
+    </a></li>`;
+}
+elGallery.innerHTML = gallaryList; 
+//Install simplelightbox
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+import Vimeo from "@vimeo/player"
+const iframe = document.getElementById("vimeo-player");
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', function(data) {
+   console.log(data)
+});
