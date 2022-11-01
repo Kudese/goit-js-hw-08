40664dcf46268fbddf176f8bf04f61c901e5339a
Kudese <d.kuprichenkov@gmail.com>
2022-11-01 20:18:24 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,13 @@
-
 import Player from '@vimeo/player';
+import loDash from 'lodash.throttle';
+const player = new Player('vimeo-player', {});
 
-const player = new Player('vimeo-player', {
+const saveTime = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
 
-});
-
-player.on('timeupdate', function(time) {
-    console.log(time);
-});
+player.on('timeupdate', loDash(saveTime, 1000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
