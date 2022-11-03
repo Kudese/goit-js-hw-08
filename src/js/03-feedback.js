bb40f66472e64533b955f8c14c017800d03c540f
Kudese <d.kuprichenkov@gmail.com>
2022-11-03 12:25:08 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const throttle = require("lodash.throttle");
 
+import throttle from "lodash.throttle";
+const elSubButton = document.querySelector('.feedback-form');
+const elEmailField = document.querySelector('input[name=email]');
+const elMessageField = document.querySelector('textarea[name=message]');
+const localKey = 'feedback-form-state';
+
+elEmailField.addEventListener('input', throttle(saveMail, 500));
+elMessageField.addEventListener('input', throttle(saveMessage, 500));
+elSubButton.addEventListener('submit', sellForm);
+const localSave = { email: '', message: '' };
+
+function saveMail(e) {
+  localSave.email = e.target.value;
+  localStorage.setItem(localKey, JSON.stringify(localSave));
+}
+function saveMessage(e) {
+  localSave.message = e.target.value;
+  localStorage.setItem(localKey, JSON.stringify(localSave));
+}
+
+
+
+if (localStorage.getItem(localKey) !== null) {
+  elEmailField.value = JSON.parse(localStorage.getItem(localKey)).email;
+  elMessageField.value = JSON.parse(localStorage.getItem(localKey)).message;
+}
+
+function sellForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(localKey)));
+  localStorage.clear(localKey);
+  elEmailField.value = '';
+  elMessageField.value = '';
+}
