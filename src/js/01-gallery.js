b924fa29df248391983655efe4da851936bf008c
Kudese <d.kuprichenkov@gmail.com>
2022-11-03 15:25:32 +0200

    в

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,23 +4,51 @@ import { galleryItems } from './gallery-items';
 
 // console.log(galleryItems);
 
-import SimpleLightbox from "simplelightbox"
+import SimpleLightbox from 'simplelightbox';
 
-import "simplelightbox/dist/simple-lightbox.min.css"
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const elGallery = document.querySelector(".gallery");
+const elGallery = document.querySelector('.gallery');
+
 
 //Create gallary in DOM
-let gallaryList = "";
+let gallaryList = '';
 for (let i = 0; i < galleryItems.length; i++) {
   const { preview, original, description } = galleryItems[i];
-  gallaryList += `<li><a classs='gallery__item' href='${original}' >
+  gallaryList += `<a classs="gallery__item" href="${original}" >
     <img class="gallery__image"  src="${preview}" alt="${description}">
-    </a></li>`;
+    </a>`;
 }
-elGallery.innerHTML = gallaryList; 
+elGallery.innerHTML = gallaryList;
 //Install simplelightbox
-let lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
   captionDelay: 250,
 });
+
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// let galleryEl = document.querySelector('.gallery');
+// galleryEl.classList.add('gallery');
+
+// let galleryList = ``;
+
+// function initGallery() {
+//   galleryItems.forEach(element => {
+//     galleryList += `<a class="gallery__item" href="${element.original}">
+//                         <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+//                         </a>`;
+//   });
+//   galleryEl.innerHTML = galleryList;
+
+//   let lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+// }
+
+// initGallery();
